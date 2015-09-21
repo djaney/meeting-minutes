0c718073fdae49f07592e08fee2261d1820d66e1
Djane Rey Mabelin <d.mabelin@arcanys.com>
2015-09-21 18:51:23 +0800

    create routes and controller

---
+++ src/AppBundle/ApiController/MeetingMinutesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\ApiController;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class MeetingMinutesController extends BaseApiController
+{
+    public function cgetAction($meetingId){
+    }
+
+    public function postAction($meetingId,Request $req){
+    }
+
+    public function patchAction($meetingId,$id,Request $req){
+    }
+}
